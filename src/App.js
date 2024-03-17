2480758c13a294bcff866af5bec94d027eb35829
Shabbeer <shabbeer@Shabbeers-MacBook-Air.local>
2024-03-17 23:45:09 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import './App.css';
-import Navbar from './Component/Navbar/Navbar';
-import Home from './Pages/Home/Home';
+import { Component } from "react";
+import "./App.css";
+import Navbar from "./Component/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Home />
+export default class App extends Component {
+  pageSize = 15;
+  render(){
+
+    return (
+      <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home key="general" pageSize={this.pageSize} country="in" category="general" />} />
+          <Route path="/business" exact element={<Home key="business" pageSize={this.pageSize} country="in" category="business" />} />
+          <Route path="/entertainment" exact element={<Home key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />} />
+          <Route path="/general" exact element={<Home key="general" pageSize={this.pageSize} country="in" category="general" />} />
+          <Route path="/health" exact element={<Home key="health" pageSize={this.pageSize} country="in" category="health" />} />
+          <Route path="/science" exact element={<Home key="science" pageSize={this.pageSize} country="in" category="science" />} />
+          <Route path="/sports" exact element={<Home key="sports" pageSize={this.pageSize} country="in" category="sports" />} />
+          <Route path="/technology" exact element={<Home key="technology" pageSize={this.pageSize} country="in" category="technology" />} />
+        </Routes>
+      </Router>
+     
     </>
   );
 }
+}
 
-export default App;
--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -2,20 +2,24 @@ import React, { Component } from 'react'
 
 export default class Card extends Component {
   render() {
-    let {key, title,description, imageUrl , newsUrl} = this.props
+    let { key, title, description, imageUrl, newsUrl, author, date, source } = this.props
     return (
-        <>
+      <>
         <div>
-            <div className="card " style={{width: "18rem"}} key={key}> 
-                <img src={imageUrl} className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description}</p>
-                    <a href={newsUrl} className="btn btn-primary">Read more</a>
-                </div>
+          <div className="card " key={key}>
+            <img src={imageUrl ? imageUrl : "https://images.moneycontrol.com/static-mcnews/2024/02/market_up_higher-1-770x433.jpg"} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{title}
+              <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
+                {source}
+              </span></h5>
+              <p className="card-text">{description}</p>
+              <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
+              <a href={newsUrl} className="btn btn-dark">Read more</a>
             </div>
+          </div>
         </div>
-        </>
+      </>
     )
   }
 }
